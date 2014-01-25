223baea44d7a2729856c21d1fc19a7d7bb2407bb
Wong Yong Jie <yjwong92@gmail.com>
2014-01-26 06:57:39 +0800

    Microphone seems to be picking up audio NAO!

--- src/sg/edu/nus/micphone/client/DiscoverDialogFragment.java
+++ src/sg/edu/nus/micphone/client/DiscoverDialogFragment.java
@@ -1,11 +1,19 @@
 package sg.edu.nus.micphone.client;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceListener;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EFragment;
 
@@ -16,12 +24,12 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.net.nsd.NsdManager;
-import android.net.nsd.NsdManager.ResolveListener;
-import android.net.nsd.NsdServiceInfo;
+import android.net.rtp.AudioCodec;
 import android.net.rtp.AudioGroup;
 import android.net.rtp.AudioStream;
 import android.net.rtp.RtpStream;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.MulticastLock;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,37 +41,35 @@ import android.widget.TextView;
 @EFragment
 public class DiscoverDialogFragment extends DialogFragment {
 	private static final String TAG = "DiscoverDialogFragment";
-	private static final String SERVICE_TYPE = "_rtp._udp.";
+	private static final String SERVICE_TYPE = "_rtp._udp.local.";
 	private static String SERVICE_NAME = "KboxService";
 	
-	/** Service discovery */
-	private NsdManager mNsdManager;
-	private NsdManager.DiscoveryListener mDiscoveryListener;
-	private ResolveListener mResolveListener;
-	private boolean mPerformingDiscovery = false;
+	/** WiFi stuff */
+	private WifiManager mWifiManager;
+	private MulticastLock mMulticastLock;
+	private static final String WIFI_LOCK_NAME = "KboxServiceWifiLock";
 	
-	private static List<NsdServiceInfo> mDiscoveredServicesList;
-	private static NsdServiceInfoListAdapter mDiscoveredServicesListAdapter;
+	/** JmDNS */
+	private JmDNS mJmDNS;
+	private ServiceListener mDiscoveryListener;
+	private List<ServiceEvent> mDiscoveredServicesList;
+	private ServiceEventListAdapter mDiscoveredServicesListAdapter;
 	
 	/** Audio management */
-	private static AudioStream mMicStream;
-	private static AudioGroup mStreamGroup;
+	private AudioStream mMicStream;
+	private AudioGroup mStreamGroup;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		// Create the list adapter.
-		mDiscoveredServicesList = new ArrayList<NsdServiceInfo>();
-		mDiscoveredServicesListAdapter = new NsdServiceInfoListAdapter(getActivity(), mDiscoveredServicesList);
-		
-		// Obtain the Network Service Discovery Manager.
-		mNsdManager = (NsdManager) getActivity().getSystemService(Context.NSD_SERVICE);
+		// Obtain the WiFi manager.
+		mWifiManager = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
 		
 		// Create the discovery listener.
-		initializeDiscoveryListener();
-		initializeResolveListener();
-		startDiscovery();
+		mDiscoveredServicesList = new ArrayList<ServiceEvent>();
+		mDiscoveredServicesListAdapter = new ServiceEventListAdapter(getActivity(), mDiscoveredServicesList);
+		initializeServiceListener();
 	}
 	
 	@Override
@@ -74,28 +80,173 @@ public class DiscoverDialogFragment extends DialogFragment {
 			.setAdapter(mDiscoveredServicesListAdapter, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					NsdServiceInfo serviceInfo = mDiscoveredServicesList.get(which);
-					Log.d(TAG, "Selected server: " + serviceInfo.getHost().getHostAddress());
+					ServiceEvent ev = mDiscoveredServicesList.get(which);
+					Log.d(TAG, "Selected server: " + ev.getInfo().getHostAddresses()[0]);
 					
 					// Obtain the endpoint port and host.
-					int port = serviceInfo.getPort();
-					InetAddress host = serviceInfo.getHost();
+					int port = ev.getInfo().getPort();
+					InetAddress host = ev.getInfo().getInetAddresses()[0];
 					beginAudioStream(host, port);
 				}
 			});
-		
 		return builder.create();
 	}
 	
 	@Override
-	public void onDismiss(DialogInterface dialog) {
-		super.onDismiss(dialog);
+	public void onResume() {
+		super.onResume();
+		
+		// Acquire a multicast lock for receiving multicast packets.
+		Log.d(TAG, "Acquiring WiFi multicast lock");
+		mMulticastLock = mWifiManager.createMulticastLock(WIFI_LOCK_NAME);
+		mMulticastLock.setReferenceCounted(true);
+		mMulticastLock.acquire();
+		
+		// Start discovering.
+		startDiscovery();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		
+		// Stop service discovery.
 		stopDiscovery();
 		
 		// Clear the list of discovered services.
 		mDiscoveredServicesList.clear();
+		
+		// Release the multicast lock.
+		Log.d(TAG, "Releasing WiFi multicast lock");
+		mMulticastLock.release();
 	}
 	
+	@Background
+	protected void startDiscovery() {
+		try {
+			InetAddress ipAddress;
+			if (isWiFiAPEnabled()) {
+				// Hackish assumption! But seems like all have 192.168.43.1.
+				ipAddress = InetAddress.getByName("192.168.43.1");
+				
+			} else {
+				// We need to obtain the address to broadcast on this interface.
+				int ipAddressInt = mWifiManager.getConnectionInfo().getIpAddress();
+				if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+					ipAddressInt = Integer.reverseBytes(ipAddressInt);
+				}
+				
+				byte[] ipByteArray = BigInteger.valueOf(ipAddressInt).toByteArray();
+				try {
+					ipAddress = InetAddress.getByAddress(ipByteArray);
+				} catch (UnknownHostException e) {
+					Log.e(TAG, "Unable to get host address");
+					ipAddress = null;
+				}
+			}
+			
+			// Create a JMDNS instance. 
+			mJmDNS = JmDNS.create(ipAddress, SERVICE_NAME);
+			mJmDNS.addServiceListener(SERVICE_TYPE, mDiscoveryListener);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Background
+	protected void stopDiscovery() {
+		mJmDNS.removeServiceListener(SERVICE_TYPE, mDiscoveryListener);
+		mJmDNS.unregisterAllServices();
+		
+		try {
+			mJmDNS.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void initializeServiceListener() {
+		mDiscoveryListener = new ServiceListener() {
+			@Override
+			public void serviceAdded(ServiceEvent ev) {
+				// A service was found!  Do something with it.
+				Log.d(TAG, "Service added: " + ev);
+				if (!ev.getType().equals(SERVICE_TYPE)) {
+					// Service type is the string containing the protocol and
+	                // transport layer for this service.
+					Log.d(TAG, "Unknown service type: " + ev.getType());
+				} else if (ev.getName().startsWith(SERVICE_NAME)) {
+					mJmDNS.requestServiceInfo(ev.getType(), ev.getName(), 1);
+				}
+			}
+
+			@Override
+			public void serviceRemoved(ServiceEvent ev) {
+				Log.d(TAG, "Service lost: " + ev);
+				for (ServiceEvent event : mDiscoveredServicesList) {
+					if (event.getName().equals(ev.getName())) {
+						mDiscoveredServicesList.remove(event);
+					}
+				}
+				
+				getActivity().runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						mDiscoveredServicesListAdapter.notifyDataSetChanged();
+					}
+				});
+			}
+
+			@Override
+			public void serviceResolved(ServiceEvent ev) {
+				Log.d(TAG, "Service resolved: " + ev);
+				
+				// Check for existing service in the list.
+				for (ServiceEvent event : mDiscoveredServicesList) {
+					if (event.getName().equals(ev.getName())) {
+						mDiscoveredServicesList.remove(event);
+					}
+				}
+				
+				mDiscoveredServicesList.add(ev);
+				
+				getActivity().runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						mDiscoveredServicesListAdapter.notifyDataSetChanged();
+					}
+				});
+			}
+		};
+	}
+	
+	/**
+	 * Utility function to check if WiFi AP is enabled on the device.
+	 */
+	private boolean isWiFiAPEnabled() {
+		boolean isWiFiAPEnabled = false;
+		Method[] wmMethods = mWifiManager.getClass().getDeclaredMethods();
+        for (Method method : wmMethods){
+            if(method.getName().equals("isWifiApEnabled")) {  
+                try {
+                	isWiFiAPEnabled = (Boolean) method.invoke(mWifiManager);
+                } catch (IllegalArgumentException e) {
+                  e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                  e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                  e.printStackTrace();
+                }
+            }
+        }
+        
+        return isWiFiAPEnabled;
+	}
+	
+
 	@Background
 	protected void beginAudioStream(InetAddress host, int port) {
 		Log.d(TAG, "beginAudioStream to " + host.getHostAddress() + ":" + port);
@@ -113,6 +264,7 @@ public class DiscoverDialogFragment extends DialogFragment {
 				// Connecting and sending stream.
 				mMicStream.setMode(RtpStream.MODE_SEND_ONLY);
 				mMicStream.associate(host, port);
+				mMicStream.setCodec(AudioCodec.GSM_EFR);
 				mMicStream.join(mStreamGroup);
 				
 			} catch (UnknownHostException e) {
@@ -126,126 +278,11 @@ public class DiscoverDialogFragment extends DialogFragment {
 		}
 	}
 	
-	public void startDiscovery() {
-		// Begin to discover network services.
-		mNsdManager.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
-		mPerformingDiscovery = true;
-	}
-	
-	public void stopDiscovery() {
-		if (mPerformingDiscovery) {
-			mNsdManager.stopServiceDiscovery(mDiscoveryListener);
-			mPerformingDiscovery = false;
-		}
-	}
-	
-	public void initializeDiscoveryListener() {
-		Log.d(TAG, "Initializing discovery listener");
-		
-		// Instantiate a new DiscoveryListener.
-		mDiscoveryListener = new NsdManager.DiscoveryListener() {
-			
-			@Override
-			public void onStopDiscoveryFailed(String serviceType, int errorCode) {
-				Log.e(TAG, "Discovery failed: Error code: " + errorCode);
-				mNsdManager.stopServiceDiscovery(this);
-			}
-			
-			@Override
-			public void onStartDiscoveryFailed(String serviceType, int errorCode) {
-				Log.e(TAG, "Discovery failed: Error code: " + errorCode);
-				mNsdManager.stopServiceDiscovery(this);
-			}
-			
-			@Override
-			public void onServiceLost(NsdServiceInfo service) {
-				// When the network service is no longer available.
-				// Internal bookkeeping code here.
-				Log.e(TAG, "Service lost: " + service);
-				for (NsdServiceInfo serviceInfo : mDiscoveredServicesList) {
-					if (serviceInfo.getServiceName().equals(service.getServiceName())) {
-						mDiscoveredServicesList.remove(serviceInfo);
-					}
-				}
-				
-				getActivity().runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						mDiscoveredServicesListAdapter.notifyDataSetChanged();
-					}
-				});
-			}
-			
-			@Override
-			public void onServiceFound(NsdServiceInfo service) {
-				// A service was found!  Do something with it.
-				Log.d(TAG, "Service discovery success: " + service);
-				if (!service.getServiceType().equals(SERVICE_TYPE)) {
-					// Service type is the string containing the protocol and
-	                // transport layer for this service.
-					Log.d(TAG, "Unknown service type: " + service.getServiceType());
-				} else if (service.getServiceName().startsWith(SERVICE_NAME)) {
-					mNsdManager.resolveService(service, mResolveListener);
-				}
-			}
-			
-			@Override
-			public void onDiscoveryStopped(String serviceType) {
-				Log.i(TAG, "Discovery stopped: " + serviceType);
-			}
-			
-			// Called as soon as service discovery begins.
-			@Override
-			public void onDiscoveryStarted(String regType) {
-				Log.d(TAG, "Service discovery started");
-			}
-		};
-	}
-	
-	public void initializeResolveListener() {
-		Log.d(TAG, "Initializing resolve listener");
-		
-		// Instantiate a new ResolveListener.
-		mResolveListener = new NsdManager.ResolveListener() {
-			
-			@Override
-			public void onServiceResolved(NsdServiceInfo serviceInfo) {
-				Log.e(TAG, "Resolve succeeded: " + serviceInfo);
-				mDiscoveredServicesList.add(serviceInfo);
-				updateAdapter();
-			}
-			
-			@Override
-			public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
-				// Called when the resolve fails. Use the error code to debug.
-				Log.e(TAG, "Resolve failed: Error code: " + errorCode);
-			}
-			
-			/**
-			 * Updates the discovered services list.
-			 * Notifies the adapter that the data set has changed.
-			 * This needs to be run on a separate (UI) thread.
-			 */
-			private void updateAdapter() {
-				getActivity().runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						mDiscoveredServicesListAdapter.notifyDataSetChanged();
-					}
-				});
-			}
-		};
-	}
-	
-	
-	/**
-	 * A list adapter for displaying discovered services in a ListView.
-	 */
-	public class NsdServiceInfoListAdapter extends ArrayAdapter<NsdServiceInfo> {
+	public class ServiceEventListAdapter extends ArrayAdapter<ServiceEvent> {
 		private Context mContext;
-		private List<NsdServiceInfo> mValues;
+		private List<ServiceEvent> mValues;
 
-		public NsdServiceInfoListAdapter(Context context, List<NsdServiceInfo> values) {
+		public ServiceEventListAdapter(Context context, List<ServiceEvent> values) {
 			super(context, android.R.layout.simple_list_item_1);
 			mContext = context;
 			mValues = values;
@@ -257,25 +294,25 @@ public class DiscoverDialogFragment extends DialogFragment {
 		}
 		
 		@Override
-		public NsdServiceInfo getItem(int position) {
+		public ServiceEvent getItem(int position) {
 			return mValues.get(position);
 		}
 		
 		@Override
-		public View getView(int positon, View convertView, ViewGroup parent) {
+		public View getView(int position, View convertView, ViewGroup parent) {
 			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			View rowView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
 			
 			TextView textView = (TextView) rowView.findViewById(android.R.id.text1);
-			NsdServiceInfo serviceInfo = mValues.get(positon);
+			ServiceEvent serviceInfo = mValues.get(position);
 			
 			// Sample format: KboxService 2 (192.168.1.1:32132)
 			// The \032 is actually a space (base 10 ASCII), but it doesn't
 			// seem to be processed correctly by Android API.
 			textView.setText(
-					serviceInfo.getServiceName().replace("\\032", " ") + " (" +
-					mValues.get(positon).getHost().getHostAddress() + ":" +
-					mValues.get(positon).getPort() + ")");
+					serviceInfo.getName() + " (" +
+					mValues.get(position).getInfo().getHostAddresses()[0] + ":" +
+					mValues.get(position).getInfo().getPort() + ")");
 			return rowView;
 		}
 	}
