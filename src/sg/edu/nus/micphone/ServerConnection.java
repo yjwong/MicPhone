71ba82012db3462d0609d62c257fb1f60aad76e5
Du Yanxian <duyanxian1990@gmail.com>
2014-01-25 18:26:03 +0800

    Added AudioCodec configuration in ServerConnection

--- src/sg/edu/nus/micphone/ServerConnection.java
+++ src/sg/edu/nus/micphone/ServerConnection.java
@@ -4,27 +4,26 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import android.net.rtp.AudioCodec;
 import android.net.rtp.AudioGroup;
 import android.net.rtp.AudioStream;
 import android.net.rtp.RtpStream;
-import android.os.Handler;
 import android.util.Log;
 
-/**Construct : ServerConnection(Handler, AudioGroup<singleton>) -> */
+/** Construct : ServerConnection(Handler, AudioGroup<singleton>) -> */
 public class ServerConnection {
 
 	private static final String TAG = "ServerConnection";
+	private static final AudioCodec CODEC = AudioCodec.GSM_EFR;
 
 	private AudioGroup m_outAudio;
-	private Handler m_UpdateHandler;
 	private Socket m_Socket;
 	private ReceivingServer m_receiveServer;
 	private int m_Port = -1;
 
-	public ServerConnection(Handler handler, AudioGroup outAudio) {
-		this.m_UpdateHandler = handler;
+	public ServerConnection(AudioGroup outAudio) {
 		this.m_outAudio = outAudio;
-		this.m_receiveServer = new ReceivingServer(m_UpdateHandler);
+		this.m_receiveServer = new ReceivingServer();
 	}
 
 	public void tearDown() {
@@ -39,15 +38,11 @@ public class ServerConnection {
 		m_Port = port;
 	}
 
-	private Socket getSocket() {
-		return m_Socket;
-	}
-
 	private class ReceivingServer {
 		ServerSocket m_ServerSocket = null;
 		Thread m_Thread = null;
 
-		public ReceivingServer(Handler handler) {
+		public ReceivingServer() {
 			m_Thread = new Thread(new ServerThread());
 			m_Thread.start();
 		}
@@ -66,18 +61,19 @@ public class ServerConnection {
 			@Override
 			public void run() {
 
-				try {				
+				try {
 					m_ServerSocket = new ServerSocket(0);
 					setLocalPort(m_ServerSocket.getLocalPort());
 
 					while (!Thread.currentThread().isInterrupted()) {
 						Log.d(TAG, "ServerSocket Created, awaiting connection");
 						m_Socket = m_ServerSocket.accept();
-						Log.d(TAG, "Connected.");
 
-						Log.d(TAG, "Joining AudioGroup");
+						Log.d(TAG, "Connected. Joining AudioGroup");
+
 						AudioStream incoming = new AudioStream(
 								m_Socket.getInetAddress());
+						incoming.setCodec(CODEC);
 						incoming.setMode(RtpStream.MODE_RECEIVE_ONLY);
 						incoming.join(m_outAudio);
 						Log.d(TAG, "Joined AudioGroup");
