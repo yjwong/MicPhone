b024aadbff554b63cdad07aaa406833b779579a8
Du Yanxian <duyanxian1990@gmail.com>
2014-01-26 11:21:31 +0800

    Migrated getInetAddress() from ConnectionHandler to Network Utils

--- src/sg/edu/nus/micphone/NetworkUtils.java
+++ src/sg/edu/nus/micphone/NetworkUtils.java
@@ -4,8 +4,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteOrder;
+import java.util.Collections;
+import java.util.List;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
@@ -13,30 +18,32 @@ import android.util.Log;
 
 public class NetworkUtils {
 	private static final String TAG = "NetworkUtils";
+	private static final String WIFI_AP_IP = "192.168.43.1";
 
 	public NetworkUtils() {
-		
+
 	}
-	
+
 	public static InetAddress getLocalAddress(Context context) {
-		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		WifiManager wifiManager = (WifiManager) context
+				.getSystemService(Context.WIFI_SERVICE);
 		InetAddress ipAddress = null;
 		if (isWiFiAPEnabled(context)) {
 			// Hackish assumption! But seems like all have 192.168.43.1.
 			try {
-				ipAddress = InetAddress.getByName("192.168.43.1");
+				ipAddress = InetAddress.getByName(WIFI_AP_IP);
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		} else {
 			// We need to obtain the address to broadcast on this interface.
 			int ipAddressInt = wifiManager.getConnectionInfo().getIpAddress();
 			if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
 				ipAddressInt = Integer.reverseBytes(ipAddressInt);
 			}
-			
+
 			byte[] ipByteArray = BigInteger.valueOf(ipAddressInt).toByteArray();
 			try {
 				ipAddress = InetAddress.getByAddress(ipByteArray);
@@ -45,31 +52,54 @@ public class NetworkUtils {
 				ipAddress = null;
 			}
 		}
-		
+
 		return ipAddress;
 	}
-	
+
 	/**
 	 * Utility function to check if WiFi AP is enabled on the device.
 	 */
 	public static boolean isWiFiAPEnabled(Context context) {
-		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		WifiManager wifiManager = (WifiManager) context
+				.getSystemService(Context.WIFI_SERVICE);
 		boolean isWiFiAPEnabled = false;
 		Method[] wmMethods = wifiManager.getClass().getDeclaredMethods();
-        for (Method method : wmMethods){
-            if(method.getName().equals("isWifiApEnabled")) {  
-                try {
-                	isWiFiAPEnabled = (Boolean) method.invoke(wifiManager);
-                } catch (IllegalArgumentException e) {
-                  e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                  e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                  e.printStackTrace();
-                }
-            }
-        }
-        
-        return isWiFiAPEnabled;
+		for (Method method : wmMethods) {
+			if (method.getName().equals("isWifiApEnabled")) {
+				try {
+					isWiFiAPEnabled = (Boolean) method.invoke(wifiManager);
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return isWiFiAPEnabled;
+	}
+
+	public static InetAddress getInetAddress() {
+		try {
+			List<NetworkInterface> netInt = Collections.list(NetworkInterface
+					.getNetworkInterfaces());
+			for (NetworkInterface net : netInt) {
+				if (net.isUp() && !net.isLoopback()) {
+					List<InterfaceAddress> inetInt = net
+							.getInterfaceAddresses();
+					for (InterfaceAddress inet : inetInt) {
+						if (inet.getAddress() != null
+								&& inet.getAddress().toString().contains(".")) {
+							return inet.getAddress();
+						}
+					}
+				}
+			}
+		} catch (SocketException se) {
+			se.printStackTrace();
+		}
+		return null;
 	}
 }
--- src/sg/edu/nus/micphone/server/ConnectionHandler.java
+++ src/sg/edu/nus/micphone/server/ConnectionHandler.java
@@ -8,14 +8,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
-import java.net.InterfaceAddress;
-import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.Collections;
-import java.util.List;
 
+import sg.edu.nus.micphone.NetworkUtils;
 import android.content.Context;
 import android.media.AudioManager;
 import android.net.rtp.AudioCodec;
@@ -27,33 +23,36 @@ public class ConnectionHandler {
 	private static final String TAG = "ConnectionHandler";
 	private ServerSocket mServerSocket;
 	private boolean mRunning = false;
-	
+
 	private AudioGroup mAudioGroup;
 	private Context mContext;
 
 	/**
 	 * Handles and negotiates potential RTP connections.
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
-	public ConnectionHandler(Context context, AudioGroup audioGroup) throws IOException {
+	public ConnectionHandler(Context context, AudioGroup audioGroup)
+			throws IOException {
 		Log.d(TAG, "ConnectionHandler started");
 		mServerSocket = new ServerSocket(0);
 		mContext = context;
 		mAudioGroup = audioGroup;
 	}
-	
+
 	public void start() {
 		mRunning = true;
-		
+
 		// Connection handling loop.
 		while (true) {
 			Socket singleConnection;
 			try {
 				singleConnection = mServerSocket.accept();
-				SingleConnectionHandler handler = new SingleConnectionHandler(singleConnection);
+				SingleConnectionHandler handler = new SingleConnectionHandler(
+						singleConnection);
 				Thread thread = new Thread(handler);
 				thread.start();
-				
+
 				// Stop the loop when requested.
 				if (!mRunning) {
 					try {
@@ -62,100 +61,82 @@ public class ConnectionHandler {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
+
 					return;
 				}
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	public int getLocalPort() {
 		return mServerSocket.getLocalPort();
 	}
-	
+
 	public void terminate() {
 		mRunning = false;
 	}
-	
+
 	/**
 	 * Handles a single connection.
 	 */
 	public class SingleConnectionHandler implements Runnable {
 		private static final String TAG = "SingleConnectionHandler";
 		private Socket mSocket;
-		
+
 		public SingleConnectionHandler(Socket socket) {
 			mSocket = socket;
 			Log.d(TAG, "Handling connection from " + socket.getInetAddress());
 		}
-		
+
 		@Override
 		public void run() {
 			try {
 				// Negotiate the RTP endpoints of the client.
 				InputStream inputStream = mSocket.getInputStream();
-				BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+				BufferedReader inputReader = new BufferedReader(
+						new InputStreamReader(inputStream));
 				String input = inputReader.readLine();
-				
+
 				InetAddress remoteAddress = mSocket.getInetAddress();
 				int remotePort = Integer.parseInt(input);
-				
-				InetAddress localAddress = this.getInetAddress();
+
+				InetAddress localAddress = NetworkUtils.getInetAddress();
 				AudioStream audioStream = new AudioStream(localAddress);
 				int localPort = audioStream.getLocalPort();
-				
+
 				OutputStream outputStream = mSocket.getOutputStream();
-				BufferedWriter outputWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+				BufferedWriter outputWriter = new BufferedWriter(
+						new OutputStreamWriter(outputStream));
 				outputWriter.write(Integer.toString(localPort) + "\n");
 				outputWriter.flush();
-				
+
 				mSocket.close();
-				
+
 				// Associate with client RTP endpoint.
 				audioStream.setCodec(AudioCodec.GSM_EFR);
 				audioStream.setMode(AudioStream.MODE_RECEIVE_ONLY);
 				audioStream.associate(remoteAddress, remotePort);
 				audioStream.join(mAudioGroup);
-				
+
 				// Print debug information about group.
-				Log.d(TAG, "Local addr: " + audioStream.getLocalAddress() + ":" + audioStream.getLocalPort());
-				Log.d(TAG, "Remote addr: " + audioStream.getRemoteAddress() + ":" + audioStream.getRemotePort());
-				
+				Log.d(TAG, "Local addr: " + audioStream.getLocalAddress() + ":"
+						+ audioStream.getLocalPort());
+				Log.d(TAG, "Remote addr: " + audioStream.getRemoteAddress()
+						+ ":" + audioStream.getRemotePort());
+
 				// Obtain an AudioManager.
-				AudioManager manager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+				AudioManager manager = (AudioManager) mContext
+						.getSystemService(Context.AUDIO_SERVICE);
 				manager.setSpeakerphoneOn(true);
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-		private InetAddress getInetAddress() {
-			try {
-				List<NetworkInterface> netInt = Collections.list(NetworkInterface
-						.getNetworkInterfaces());
-				for (NetworkInterface net : netInt) {
-					if (net.isUp() && !net.isLoopback()) {
-						List<InterfaceAddress> inetInt = net
-								.getInterfaceAddresses();
-						for (InterfaceAddress inet : inetInt) {
-							if (inet.getAddress() != null
-									&& inet.getAddress().toString().contains(".")) {
-								return inet.getAddress();
-							}
-						}
-					}
-				}
-			} catch (SocketException se) {
-				se.printStackTrace();
-			}
-			return null;
-		}
 	}
-
 }
