7d05d63813658a6ff81e05d5fe3046035f6af760
Du Yanxian <duyanxian1990@gmail.com>
2014-01-26 14:11:13 +0800

    Turned off mic/speakerphone for functions that don't need them.

--- src/sg/edu/nus/micphone/client/ClientService.java
+++ src/sg/edu/nus/micphone/client/ClientService.java
@@ -30,17 +30,17 @@ import android.util.Log;
 public class ClientService extends Service {
 	private static final String TAG = "ClientService";
 	private static final AudioCodec CODEC = AudioCodec.AMR;
-	
+
 	public ClientService() {
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.d(TAG, "onStartCommand");
 		InetAddress host = (InetAddress) intent.getSerializableExtra("host");
 		int port = intent.getIntExtra("port", -1);
 		beginAudioStream(host, port);
-		
+
 		return super.onStartCommand(intent, flags, startId);
 	}
 
@@ -48,53 +48,61 @@ public class ClientService extends Service {
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	/** Methods for clients */
 	@Background
 	protected void beginAudioStream(InetAddress host, int port) {
 		Log.d(TAG, "beginAudioStream to " + host.getHostAddress() + ":" + port);
 		try {
-			AudioStream micStream = new AudioStream(NetworkUtils.getLocalAddress(this));
+			AudioStream micStream = new AudioStream(
+					NetworkUtils.getLocalAddress(this));
 			int localPort = micStream.getLocalPort();
-			
+
 			try {
 				// Negotiate the RTP endpoints of the server.
 				Socket socket = new Socket(host, port);
 				OutputStream outputStream = socket.getOutputStream();
-				BufferedWriter outputWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+				BufferedWriter outputWriter = new BufferedWriter(
+						new OutputStreamWriter(outputStream));
 				outputWriter.write(Integer.toString(localPort) + "\n");
 				outputWriter.flush();
-				
+
 				InputStream inputStream = socket.getInputStream();
-				BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+				BufferedReader inputReader = new BufferedReader(
+						new InputStreamReader(inputStream));
 				String input = inputReader.readLine();
-				
+
 				int remotePort = Integer.parseInt(input);
 				socket.close();
-				
+
 				// Associate with server RTP endpoint.
 				AudioGroup streamGroup = new AudioGroup();
-				//streamGroup.setMode(AudioGroup.MODE_ECHO_SUPPRESSION);
+				// streamGroup.setMode(AudioGroup.MODE_ECHO_SUPPRESSION);
 				streamGroup.setMode(AudioGroup.MODE_NORMAL);
-				
+
 				micStream.setCodec(CODEC);
 				micStream.setMode(AudioStream.MODE_SEND_ONLY);
 				micStream.associate(host, remotePort);
 				micStream.join(streamGroup);
-				
+
 				// Print debug information about group.
-				Log.d(TAG, "Local addr: " + micStream.getLocalAddress() + ":" + micStream.getLocalPort());
-				Log.d(TAG, "Remote addr: " + micStream.getRemoteAddress() + ":" + micStream.getRemotePort());
-				
+				Log.d(TAG, "Local addr: " + micStream.getLocalAddress() + ":"
+						+ micStream.getLocalPort());
+				Log.d(TAG, "Remote addr: " + micStream.getRemoteAddress() + ":"
+						+ micStream.getRemotePort());
+
 				// Obtain an AudioManager.
-				AudioManager manager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+				AudioManager manager = (AudioManager) this
+						.getSystemService(Context.AUDIO_SERVICE);
 				manager.setMicrophoneMute(false);
-				
+				manager.setSpeakerphoneOn(false);
+				manager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
